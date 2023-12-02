ef744d488ce324b00e6370ad550bdc0bb1470f35
haukereckers <hauke.reckers@gmx.de>
2023-12-02 01:28:17 +0100

    Hallo

--- monopoly/src/App.jsx
+++ monopoly/src/App.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 
-const App = () => {
+// Hier können Sie die Spielfelder und ihre Eigenschaften definieren
+const boardData = [
+  { id: 0, name: 'Start', color: 'brown' },
+  { id: 1, name: 'Mediterranean Ave', color: 'brown' },
+  // ... Weitere Spielfelder ...
+  { id: 39, name: 'Boardwalk', color: 'blue' },
+];
+
+// Komponente für ein Spielfeld
+const BoardSquare = ({ name, color }) => (
+  <div className={`board-square ${color}`}>
+    {name}
+  </div>
+);
+
+// Komponente für das Monopoly-Brett
+const MonopolyBoard = () => (
+  <div className="monopoly-board">
+    {boardData.map((square) => (
+      <BoardSquare key={square.id} name={square.name} color={square.color} />
+    ))}
+  </div>
+);
+
+function App() {
   return (
-    <div>App</div>
-  )
+    <div className="App">
+      <h1>Monopoly Board</h1>
+      <MonopolyBoard />
+    </div>
+  );
 }
 
 export default App;
